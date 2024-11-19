fc247b6b89872d190d5fed81b6d6f9b37c7b7785
artemisfowl <skb655952@gmail.com>
2024-11-19 08:13:13 +0530

    Added necessary header files

---
+++ inc/vman.h
@@ -0,0 +1,33 @@
+/**
+ * @file vman.h
+ * @brief Header file containing the basic usage routines.
+ *
+ * @description This header file will be containing all the routines to be
+ * used specifically for this program.
+ *
+ * @author oldgod
+ * @date Tue, 19 Nov 2024 06:58:52 +0530
+ * @bug No known bugs as of now.
+ */
+
+/**
+ * @brief Function to show the usage of this program.
+ *
+ * This function can be used in order to show the options available for this
+ * program.
+ *
+ * @param void - no arguments will be accepted by this program.
+ *
+ * @note The help information will be updated as and when the development goes
+ * ahead for this program.
+ */
+void vman_usage(void);
+
+/**
+ * @brief Function to show the version of this program.
+ *
+ * This function can be used in order to show the program version.
+ *
+ * @param void - no arguments will be accepted by this program.
+ */
+void vman_version(void);
